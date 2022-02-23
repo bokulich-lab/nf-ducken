529663d5e248be9529a93ffbfdba18a3ad78ac6f
lina-kim <lina.kim@hest.ethz.ch>
2022-02-23 11:03:19 +0100

    Initial commit of basic base.config, main.nf, and lib/Workflow16s.groovy files for setup.

---
+++ lib/Workflow16S.groovy
@@ -0,0 +1,11 @@
+//
+// This file holds several functions specific to the workflow/16s-pipe in the nf-core/16s-pipe pipeline
+//
+
+class Workflow16S {
+    //
+    // Check and validate parameters
+    //
+    public static void initialise(params, log) {
+    }
+}
