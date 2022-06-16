fa4ed8cb0961568d36240dcee91e799f0f60a225
lina-kim <lina.kim@hest.ethz.ch>
2022-06-16 17:07:10 +0200

    Implement per-sample FASTQ finder for manifest creation

--- bin/make_manifest.py
+++ bin/make_manifest.py
@@ -5,19 +5,65 @@ Generates a manifest for FASTQ files based on file presence.
 
 from pathlib import Path
 import argparse
-import glob
+import logging
 import pandas as pd
+import re
 
 
-HEADER_DICT = {"single": "sample-id\tabsolute-filepath",
-               "paired": "sample-id\tforward-absolute-filepath\treverse"
-                         "-absolute-filepath"}
+HEADER_DICT = {"single": ["sample-id", "absolute-filepath"],
+               "paired": ["sample-id, forward-absolute-filepath",
+                          "reverse-absolute-filepath"]}
+
+NUM_DICT = {"single": 1,
+            "paired": 2}
 
 
-def split_manifest(inp_manifest: pd.DataFrame,
-                   out_dir: str,
-                   suffix_str: str,
-                   split_method: str) -> None:
+def get_sample_ids(inp_dir: str,
+                   read_type: str,
+                   suffix: str) -> pd.DataFrame:
+    """
+    Retrieves FASTQ file paths per sample ID.
+
+    :param inp_dir:
+    :param read_type:
+    :param suffix:
+    :return:
+    """
+    fastq_path_list = sorted(Path(inp_dir).resolve().glob(f"*{suffix}"))
+    assert len(list(fastq_path_list)) > 0, \
+        f"No files were found in {inp_dir} matching the suffix {suffix}! " \
+        f"Exiting..."
+
+    # Get all FASTQs
+    fname_df = pd.DataFrame(fastq_path_list,
+                            index=None,
+                            columns=["file_path"])
+    fname_df["sample_id"] = fname_df["file_path"].apply(
+        lambda x: re.search(f"(\w+)({suffix})", str(x)).group(1)
+    )
+
+    # Group by sample ID
+    group = fname_df.groupby("sample_id")
+    sample_df = group.aggregate(list)
+
+    # Filter out samples with incorrect number of FASTQs
+    sample_df["num_fastq"] = sample_df.iloc[:, 0].apply(len)
+    num_mismatch_fq = sum(sample_df["num_fastq"] != NUM_DICT[read_type])
+    if num_mismatch_fq > 0:
+        logging.warning(f"There is/are {num_mismatch_fq} sample(s) with the "
+                        f"incorrect number of FASTQs!")
+    assert num_mismatch_fq != sample_df.shape[0], \
+        f"There are no FASTQs matching read type {read_type}! Exiting..."
+
+    sample_df = sample_df[sample_df["num_fastq"] == NUM_DICT[read_type]]
+    sample_df.sort_values(inplace=True)
+
+    return sample_df
+
+
+def assign_fastqs_per_sample(sample_fastq_df: pd.DataFrame,
+                             suffix_dict: dict):
+
     pass
 
 
@@ -44,7 +90,7 @@ def arg_parse():
         "-o", "--output_fname",
         help="File name of output manifest.",
         type=str,
-        default="output_manifest.tsv"
+        default="fastq_manifest.tsv"
     )
     parser.add_argument(
         "--suffix",
@@ -72,6 +118,15 @@ def arg_parse():
 def main(args):
     assert Path(args.input_dir).is_dir()
     assert Path(args.output_fname).parent.is_dir()
+
+    logging.basicConfig(filename=Path(args.output_fname).parent /
+                                 "manifest.log")
+
+    suffix_dict = {"single": [args.suffix],
+                   "paired": [args.r1_suffix, args.r2_suffix]}
+
+    sample_df = get_sample_ids(args.inp_dir, args.read_type, args.suffix)
+    sample_fastq_df = assign_fastqs_per_sample(sample_df, suffix_dict)
 
 
 if __name__ == "__main__":
