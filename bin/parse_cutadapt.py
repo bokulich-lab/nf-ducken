60402ad0b4ca85b69cbe2dde0f439b3f6a54566e
lina-kim <lina.kim@hest.ethz.ch>
2024-01-22 15:57:01 +0100

    Add regex for FASTQ files and sample ID extraction

--- bin/parse_cutadapt.py
+++ bin/parse_cutadapt.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-Parses Cutadapt file to determine optimal sample use.
+Parses Cutadapt file to determine optimal sample-primer pairs.
 """
 
-from collections import Counter
 from pathlib import Path
 import argparse
 import re
 
-import numpy as np
-import pandas as pd
 
-
-def read_log(log_file: str):
+def read_log(log_file: str, read_type: str):
     # Note: Each log represents a single primer (pair)
     # Split log by sample
     # Extract summary
 
     count_samples = 0
+    flag_dict = {"--front" : "R1",
+                 "-g"      : "R1",
+                 "-G"      : "R2"}
+
+    read_dict = {"single" : 1,
+                 "paired" : 2}
+
     with open(log_file) as f:
         # Find start log sample split based on header text:
         # "This is cutadapt 4.2 with Python 3.8.15"
@@ -31,7 +34,51 @@ def read_log(log_file: str):
                         line):
                 count_samples += 1
                 in_sample = True
-            while in_sample:
+
+                # Within the sample
+                while in_sample:  # needs to be modified
+                    line = f.readline()
+                    if not line:
+                        break
+
+                    # Search for CLI params
+                    if re.match(r"Command line parameters:", line):
+                        cli_line = line
+                        print(cli_line)
+
+                        # Search for --p-front primer only
+                        # (--front or -g) in Cutadapt
+                        # OR
+                        # Search for both --p-front-f and --p-front-r primers
+                        # (--front or -g) and (-G) in Cutadapt
+                        primer_list = re.findall(
+                            r"\-\-front [A-Z]+|\-g [A-Z]+|\-G [A-Z]+", cli_line)
+                        primer_dict = {flag_dict[param_set.split()[0]]:
+                                       param_set.split()[1] for
+                                       param_set in primer_list}
+
+                        # Search for inp FASTQs, extract sample names
+                        # Input FASTQs/FASTAs have no flag
+                        # Output FASTQs/FASTAs are preceded by flags
+                        # (--output or -o) and/or (--paired-output or -p)
+                        fastq_list = re.findall(
+                            r"\-[o|p] [\S]+\.fast[\S]+|[\S]+\.fast[\S]+",
+                            cli_line)
+
+
+                    # Search for adapter summary stats
+                    if re.match(r"Total read pairs processed:", line):
+                        r1_line = f.readline()
+                        r2_line = f.readline()
+                        rstats_list = [rline.strip() for rline in [r1_line,
+                                                                   r2_line]
+                                       if re.match(r"Read [1-2] with adapter:",
+                                                   rline.strip())]
+                        assert len(rstats_list) > 0, \
+                            "The text 'Read 1/2 with adapter' could not be " \
+                            "found in the Cutadapt log for this sample!"
+
+                        break   # Look for next sample
 
                 pass
         pass
@@ -48,6 +95,16 @@ def arg_parse():
         type=str,
         required=True,
     )
+    parser.add_argument(
+        "-r",
+        "--read_type",
+        help="Read type of input FASTQs, either 'single' or 'paired'.",
+        type=str,
+        choices=["single", "paired"],
+        required=True
+    )
+
+    # Add sample/FASTQ names for an added layer of cross-reference?
 
     # Optional user-input arguments
     parser.add_argument(
@@ -56,6 +113,14 @@ def arg_parse():
         help="Location to print output files.",
         type=str,
         default=Path.cwd(),
+    )
+    parser.add_argument(
+        "-f",
+        "--fastq_suffix",
+        help="Sample FASTQ file name suffix to remove in sample assignment; "
+             "comma-delimited.",
+        type=str,
+        default="*_00_L001_R[1-2]_001.fastq.gz,*.fastq.gz",
     )
 
     args = parser.parse_args()
@@ -66,8 +131,7 @@ def main(args):
     assert Path(args.input_log).is_file()
     assert Path(args.output_dir).is_dir()
 
-    read_log(args.input_log)
-    pass
+    read_log(args.input_log, args.read_type)
 
 
 if __name__ == "__main__":
